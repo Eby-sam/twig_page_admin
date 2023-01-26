94028f16904db4e6322d1f355e7d1a5c325eeb10
sam <coquelet.samuel@gmail.com>
2023-01-26 09:32:06 +0100

    creat function C.R.U.D article

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -32,4 +32,18 @@ class ArticleController extends AbstractController {
     }
 
 
+    public function upArticle() {
+        $article = R::load('article',1);
+        $article->title = $this->dataClean($this->getFormField('title'));
+        $article->content = $this->dataClean($this->getFormField('content'));
+        $insert = R::store($article);
+        self::index();
+    }
+
+    public function deleteArticle() {
+
+        R::trash('article', 'id IN ('. R::genSlots($ids).')',$ids);
+        self::index();
+    }
+
 }
